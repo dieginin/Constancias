d2c137a5b08e92e680a10c3f7edf3f23f08f1304
dieginin <diegobalestra9@gmail.com>
2024-04-26 21:30:42 -0400

    update database.py

--- services/database.py
+++ services/database.py
@@ -7,20 +7,33 @@ from tinydb.table import Document
 class Database:
     def __init__(self) -> None:
         db = TinyDB("database.json")
-        self._clients = db.table("clients")
+        self._clientes = db.table("clientes")
 
     @property
-    def clients(self) -> List[Document]:
-        return self._clients.all()
+    def clientes(self) -> List[Document]:
+        return self._clientes.all()
 
-    def get_client(self) -> Document | List[Document] | None:
-        return self._clients.get(Query().name == "name")
+    def obtener_cliente(self) -> Document | List[Document] | None:
+        return self._clientes.get(Query().name == "name")
 
-    def insert_client(self) -> int:
-        return self._clients.insert({"name": "name"})
+    def insertar_cliente(
+        self, nombre: str, estado: str, municipio: str, localidad: str
+    ) -> str:
+        if self._clientes.contains(Query().nombre == nombre):
+            return f"{nombre} ya existe"
 
-    def modify_client(self) -> int:
-        return self._clients.update({"name": "name"}, doc_ids=[0])[0]
+        data_cliente = {
+            "uid": len(self._clientes) + 1,
+            "nombre": nombre,
+            "estado": estado,
+            "municipio": municipio,
+            "localidad": localidad,
+        }
+        self._clientes.insert(data_cliente)
+        return f"{nombre} agregado exitosamente"
 
-    def delete_client(self) -> int:
-        return self._clients.remove(doc_ids=[0])[0]
+    def modificar_cliente(self) -> int:
+        return self._clientes.update({"nombre": "nombre"}, doc_ids=[1])[0]
+
+    def borrar_cliente(self) -> int:
+        return self._clientes.remove(doc_ids=[1])[0]
