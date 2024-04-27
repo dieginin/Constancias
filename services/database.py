8a8424d977ce8a3afceee912f166b2347010817d
dieginin <diegobalestra9@gmail.com>
2024-04-27 00:03:48 -0400

    update database.py

--- services/database.py
+++ services/database.py
@@ -7,6 +7,7 @@ class Database:
     def __init__(self) -> None:
         db = TinyDB("database.json")
         self._clientes = db.table("clientes")
+        self._constancias = db.table("constancias")
 
     @property
     def clientes(self) -> list[Cliente]:
@@ -30,13 +31,14 @@ class Database:
             return f"{nombre} ya existe"
 
         data_cliente = {
-            "uid": len(self._clientes) + 1,
+            "uid": 0,
             "nombre": nombre,
             "estado": estado,
             "municipio": municipio,
             "localidad": localidad,
         }
-        self._clientes.insert(data_cliente)
+        uid = self._clientes.insert(data_cliente)
+        self._clientes.update({"uid": uid}, doc_ids=[uid])
         return f"{nombre} agregado exitosamente"
 
     def modificar_cliente(
@@ -68,4 +70,16 @@ class Database:
         cliente = self._clientes.get(doc_id=uid)
         self._clientes.remove(doc_ids=[uid])[0]
         return f"{cliente["nombre"]} eliminado"  # type: ignore
-    
+
+    def insertar_constancia(
+        self, uid: int, no_organismos: int, costo_unitario: float
+    ) -> str:
+        data_constancia = {
+            "folio": 0,
+            "uid": uid,
+            "no_organismos": no_organismos,
+            "costo_unitario": costo_unitario,
+        }
+        folio = self._constancias.insert(data_constancia)
+        self._constancias.update({"folio": folio}, doc_ids=[folio])
+        return f"Constancia {folio} agregada exitosamente"
