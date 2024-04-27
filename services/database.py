b6109434f5649c0e05cdc0214a232db7a561721d
dieginin <diegobalestra9@gmail.com>
2024-04-26 22:27:48 -0400

    update database.py

--- services/database.py
+++ services/database.py
@@ -1,7 +1,6 @@
-from typing import List
-
 from tinydb import Query, TinyDB
-from tinydb.table import Document
+
+from models import Cliente
 
 
 class Database:
@@ -10,11 +9,24 @@ class Database:
         self._clientes = db.table("clientes")
 
     @property
-    def clientes(self) -> List[Document]:
-        return self._clientes.all()
+    def clientes(self) -> list[Cliente]:
+        _clientes = []
+        for c in self._clientes.all():
+            _clientes.append(
+                Cliente(
+                    c["uid"], c["nombre"], c["estado"], c["municipio"], c["localidad"]
+                )
+            )
+        return _clientes
 
-    def obtener_cliente(self) -> Document | List[Document] | None:
-        return self._clientes.get(Query().name == "name")
+    def obtener_cliente(self, nombre: str) -> Cliente:
+        if not self._clientes.contains(Query().nombre == nombre):
+            return Cliente(0, *4*["None"])
+
+        c = self._clientes.get(Query().nombre == nombre)
+        return Cliente(
+            c["uid"], c["nombre"], c["estado"], c["municipio"], c["localidad"] # type: ignore
+        )
 
     def insertar_cliente(
         self, nombre: str, estado: str, municipio: str, localidad: str
@@ -32,8 +44,30 @@ class Database:
         self._clientes.insert(data_cliente)
         return f"{nombre} agregado exitosamente"
 
-    def modificar_cliente(self) -> int:
-        return self._clientes.update({"nombre": "nombre"}, doc_ids=[1])[0]
+    def modificar_cliente(
+        self,
+        uid: int,
+        nombre: str | None = None,
+        estado: str | None = None,
+        municipio: str | None = None,
+        localidad: str | None = None,
+    ) -> str:
+        if self._clientes.contains(Query().nombre == nombre):
+            return f"{nombre} ya existe"
 
-    def borrar_cliente(self) -> int:
-        return self._clientes.remove(doc_ids=[1])[0]
+        if nombre:
+            self._clientes.update({"nombre": nombre}, doc_ids=[uid])
+        if estado:
+            self._clientes.update({"estado": estado}, doc_ids=[uid])
+        if municipio:
+            self._clientes.update({"municipio": municipio}, doc_ids=[uid])
+        if localidad:
+            self._clientes.update({"localidad": localidad}, doc_ids=[uid])
+        cliente = self._clientes.get(doc_id=uid)
+        return f"{cliente["nombre"]} actualizado"  # type: ignore
+
+    def borrar_cliente(self, uid: int) -> str:
+        cliente = self._clientes.get(doc_id=uid)
+        self._clientes.remove(doc_ids=[uid])[0]
+        return f"{cliente["nombre"]} eliminado"  # type: ignore
+    
